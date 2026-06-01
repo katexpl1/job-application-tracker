ac047d26af260cacf0731d66b8cda3ac58e987f0
Katarzyna Kubisiak <kasia41277@gmail.com>
2026-06-01 23:21:11 +0200

    fix vercel deploy

--- app/@modal/(.)profile/page.tsx
+++ app/@modal/(.)profile/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-
-import { ProfileContent } from "@/app/profile/ProfileContent";
-import { Modal } from "blunt-ui";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
+
+const Modal = dynamic(
+  () => import("blunt-ui").then((mod) => mod.Modal),
+  { ssr: false },
+);
+
+const ProfileContent = dynamic(
+  () => import("@/app/profile/ProfileContent").then((mod) => mod.ProfileContent),
+  { ssr: false },
+);
 
 export default function ProfileModal() {
   const router = useRouter();
